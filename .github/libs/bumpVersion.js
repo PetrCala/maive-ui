b9e69686ef726998b818286d84f06e6f1e4c3062
PetrCala <cala.p@seznam.cz>
2024-01-03 10:21:43 +0100

    Version update: Previous version: 1.0.0 New version: 1.0.0-1 Setting npm version to 1.0.0-1 v1.0.0-1
    ::set-output name=NEW_VERSION::1.0.0-1

---
+++ .github/libs/bumpVersion.js
@@ -0,0 +1,44 @@
+// Source: Expensify (under MIT license)
+// Link: https://github.com/Expensify/App/blob/main/.github/actions/javascript/bumpVersion/bumpVersion.js
+
+const {promisify} = require('util');
+const fs = require('fs');
+const exec = promisify(require('child_process').exec);
+const _ = require('underscore');
+const core = require('@actions/core');
+const yargs = require('yargs/yargs')
+const versionUpdater = require('./versionUpdater');
+const {argv} = yargs(process.argv.slice(2))
+    .option('SEMVER_LEVEL', {
+        alias: 'semver',
+        describe: 'The semantic version level to increment',
+        type: 'string',
+        demandOption: true,
+    });
+
+
+// let semanticVersionLevel = core.getInput('SEMVER_LEVEL', {require: true}); // Use when running as GH action
+let semanticVersionLevel = argv.SEMVER_LEVEL; // Running the script using node.js
+if (!semanticVersionLevel || !_.contains(versionUpdater.SEMANTIC_VERSION_LEVELS, semanticVersionLevel)) {
+    console.log(`Invalid input for 'SEMVER_LEVEL': ${semanticVersionLevel}`);
+    semanticVersionLevel = versionUpdater.SEMANTIC_VERSION_LEVELS.BUILD;
+    console.log(`Defaulting to: ${semanticVersionLevel}`);
+}
+
+const {version: previousVersion} = JSON.parse(fs.readFileSync('./package.json'));
+const newVersion = versionUpdater.incrementVersion(previousVersion, semanticVersionLevel);
+console.log(`Previous version: ${previousVersion}`, `New version: ${newVersion}`);
+
+console.log(`Setting npm version to ${newVersion}`);
+exec(`npm --no-git-tag-version version ${newVersion} -m "Update version to ${newVersion}"`)
+    .then(({stdout}) => {
+        // NPM and native versions successfully updated, output new version
+        console.log(stdout);
+        core.setOutput('NEW_VERSION', newVersion);
+    })
+    .catch(({stdout, stderr}) => {
+        // Log errors and retry
+        console.log(stdout);
+        console.error(stderr);
+        core.setFailed('An error occurred in the `npm version` command');
+    });
