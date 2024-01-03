a44935e62db0808e65ece47315343e023d9e2403
PetrCala <cala.p@seznam.cz>
2024-01-03 12:25:43 +0100

    Fixed a pathing issue in the R dockerfile

--- package.R
+++
@@ -1,5 +0,0 @@
-# List of packages to install
-packages <- c("dplyr", "ggplot2", "tidyr")
-
-# Install packages
-install.packages(packages, dependencies = TRUE)
---
+++ r_scripts/packages.R
@@ -0,0 +1,5 @@
+# List of packages to install
+packages <- c("dplyr")
+
+# Install packages
+install.packages(packages, dependencies = TRUE, repos = "http://cran.rstudio.com/")
