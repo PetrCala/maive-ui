89a09c52c635b04efda0fd2bd55fc506f6c94e70
PetrCala <cala.p@seznam.cz>
2024-01-10 11:40:22 +0100

    Version update: Previous version: 0.0.5-1 New version: 0.0.5-2 Setting npm version to 0.0.5-2 Successfully updated version in setup.py to 0.0.5-2 v0.0.5-2
    ::set-output name=NEW_VERSION::0.0.5-2
    Successfully updated client npm version to 0.0.5-2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='artma',
-    version='0.0.5-1',
+    version='0.0.5-2',
     description='Automatic Replication Tools for Meta-Analysis',
     packages=find_packages(),
     install_requires=[
