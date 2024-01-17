f7f7d4519369823ed180fbab590482ef30e537a4
PetrCala <cala.p@seznam.cz>
2024-01-17 14:58:58 +0100

    Experimental changes to track R library from the container

--- R/r_scripts/main.R
+++ R/r_scripts/main.R
@@ -1,9 +1,14 @@
 # main.R
 
+# Make the container library available to R
+.libPaths(c("/usr/local/lib/R/site-library", .libPaths()))
+
 print("Hello, world!")
 
 # See: https://www.rplumber.io
-# library('plumber')
+library('plumber')
+
+print("Plumber library successfully loaded")
 
 # #* @apiTitle My API
 # pr("executables/plumber.R") %>%
