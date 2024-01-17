72c85ad482097cb36a5a86555d5d038b1c35cf1a
PetrCala <cala.p@seznam.cz>
2024-01-17 16:26:15 +0100

    Version update: Previous version: 0.0.6-0 New version: 0.0.7-0 Setting npm version to 0.0.7-0 Successfully updated version in setup.py to 0.0.7-0 v0.0.7-0
    ::set-output name=NEW_VERSION::0.0.7-0

--- Flask/setup.py
+++ Flask/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='artma',
-    version='0.0.6-0',
+    version='0.0.7-0',
     description='Automatic Replication Tools for Meta-Analysis',
     packages=find_packages(),
     install_requires=[
