40cbf52d6fb35b1e657954a9c1a4abd2df66671b
PetrCala <cala.p@seznam.cz>
2025-05-28 13:05:40 +0200

    ci: fix the TF foundation provider config

--- terraform/stacks/prod-foundation/provider.tf
+++ terraform/stacks/prod-foundation/provider.tf
@@ -1,18 +1,10 @@
 terraform {
-  required_version = "~> 1.8"
+  required_version = "~> 1.12"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.50"
     }
-  }
-
-  backend "s3" {
-    bucket         = "my-tf-state" # <- already exists from bootstrap
-    key            = "prod-foundation.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
-    dynamodb_table = "my-tf-locks"
   }
 }
 
