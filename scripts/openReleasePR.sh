39e18008ed74bf1b039b8faf5c95ed93c1a48d53
PetrCala <cala.p@seznam.cz>
2025-05-28 16:35:37 +0200

    refactor: create a new version on release

--- scripts/openReleasePR.sh
+++ scripts/openReleasePR.sh
@@ -6,6 +6,29 @@ source "$SCRIPTS_DIR/shellUtils.sh"
 
 REVIEWER="PetrCala"
 
+usage() {
+  echo "Usage: $0 --semver <SEMVER_LEVEL>"
+  exit 1
+}
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+  --semver)
+    SEMVER_LEVEL="$2"
+    shift 2
+    ;;
+  *)
+    usage
+    ;;
+  esac
+done
+
+if [ -z "$SEMVER_LEVEL" ]; then
+  error "The --semver argument is required. Must be one of {BUILD, PATCH, MINOR, MAJOR}."
+  usage
+fi
+
 if [[ $(git status --porcelain) ]]; then
   error "There are unsaved changes. Please commit or stash your changes before running this script."
   exit 1
@@ -22,6 +45,7 @@ fi
 info "Opening release PR from branch $CURRENT_BRANCH to branch $RELEASE_BRANCH..."
 
 RELEASE_VERSION=$(jq -r .version 'package.json')
+SEMVER_LEVEL=$(echo "$SEMVER_LEVEL" | tr '[:upper:]' '[:lower:]') # to lowercase
 
 gh pr create \
   --title "Release v$RELEASE_VERSION" \
@@ -29,6 +53,6 @@ gh pr create \
   --base $RELEASE_BRANCH \
   --head $CURRENT_BRANCH \
   --reviewer $REVIEWER \
-  --label "release"
+  --label "v-$SEMVER_LEVEL"
 
 success "Release PR opened!"
