6a3cd43023a88b66b8c4e7408cff6a07723f3203
PetrCala <cala.p@seznam.cz>
2025-05-28 16:07:01 +0200

    fix: modify the data bucket iam policy for the ecs task

--- terraform/stacks/prod-runtime/ecs.tf
+++ terraform/stacks/prod-runtime/ecs.tf
@@ -28,11 +28,20 @@ resource "aws_iam_policy" "data_bucket_rw" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-      { Effect = "Allow", Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"], Resource = "${data.terraform_remote_state.foundation.outputs.data_bucket_name}/*" },
-      { Effect = "Allow", Action = ["s3:ListBucket"], Resource = data.terraform_remote_state.foundation.outputs.data_bucket_name }
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        Resource = "arn:aws:s3:::${data.terraform_remote_state.foundation.outputs.data_bucket_name}/*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["s3:ListBucket"],
+        Resource = "arn:aws:s3:::${data.terraform_remote_state.foundation.outputs.data_bucket_name}"
+      }
     ]
   })
 }
+
 resource "aws_iam_role_policy_attachment" "data_bucket_rw_attach" {
   role       = aws_iam_role.task_exec.name
   policy_arn = aws_iam_policy.data_bucket_rw.arn
