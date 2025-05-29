bc4bbf7972148f52fde1b73ffe23186f5b2c59a7
PetrCala <cala.p@seznam.cz>
2025-05-29 07:04:24 +0200

    ci: minor changes

---
+++ scripts/mergePR.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MASTER_BRANCH="master"
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+gh pr merge --auto --rebase --delete-branch
+
+git checkout $MASTER_BRANCH
+git pull --rebase origin $MASTER_BRANCH
+git push origin $MASTER_BRANCH
+git branch -D $CURRENT_BRANCH
+
+echo "PR merged and branch deleted"
