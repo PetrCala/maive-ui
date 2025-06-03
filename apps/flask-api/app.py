200d1b7251929fd920dfe0797d1b6363030a4a74
PetrCala <cala.p@seznam.cz>
2025-06-03 16:56:19 +0200

    feat: remove authentication in the flask api

--- apps/flask-api/app.py
+++ apps/flask-api/app.py
@@ -7,12 +7,10 @@ def create_app():
     app = Flask(__name__)
 
     def register_blueprints(app):
-        from app.blueprints.auth import bp as auth_bp
         from app.blueprints.scripts import bp as scripts_bp
         from app.blueprints.home import bp as home_bp
         from app.blueprints.r_api import bp as r_api_bp
 
-        app.register_blueprint(auth_bp, url_prefix="/auth")
         app.register_blueprint(scripts_bp, url_prefix="/scripts")
         app.register_blueprint(home_bp, url_prefix="/")
         app.register_blueprint(r_api_bp, url_prefix="/r_api")
