525ef9bac3f010f0e7c4f4fef622a10aa2e5d475
PetrCala <cala.p@seznam.cz>
2025-06-09 16:06:02 +0200

    ci: add a budget

---
+++ terraform/stacks/prod-foundation/budget.tf
@@ -0,0 +1,28 @@
+resource "aws_budgets_budget" "monthly_limit" {
+  name         = "monthly-${var.project}-budget"
+  budget_type  = "COST"
+  limit_amount = "100"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  cost_filter {
+    name   = "LinkedAccount"
+    values = [data.aws_caller_identity.current.account_id]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    notification_type          = "ACTUAL"
+    threshold                  = 10
+    threshold_type             = "PERCENTAGE"
+    subscriber_email_addresses = [var.budget_alarm_email]
+  }
+
+  tags = {
+    Project = var.project
+  }
+}
+
+resource "aws_sns_topic" "budget_alarm" {
+  name = "${var.project}-budget-alarm-topic"
+}
