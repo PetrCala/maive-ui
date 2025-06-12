6404f8763b115e2bcc5fac838c115d5f89350684
PetrCala <cala.p@seznam.cz>
2025-06-12 11:57:47 +0200

    fix: the foundation data block key value now refers to the correct key in the runtime main.tf

--- terraform/stacks/prod-runtime/main.tf
+++ terraform/stacks/prod-runtime/main.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "foundation" {
   backend = "s3"
   config = {
     bucket = "${var.project}-tf-state"
-    key    = "prod-foundation.tfstate"
+    key    = "${var.project}/prod-foundation.tfstate"
     region = var.region
   }
 }
