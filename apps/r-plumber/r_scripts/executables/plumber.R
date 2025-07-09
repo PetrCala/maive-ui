b5e363e80b216580de7e6d649fc48f140ab02f1e
PetrCala <cala.p@seznam.cz>
2025-07-09 16:20:21 +0200

    chore: remove unused R api endpoints

--- apps/r-plumber/r_scripts/executables/plumber.R
+++ apps/r-plumber/r_scripts/executables/plumber.R
@@ -8,29 +8,11 @@ function(msg = "") {
   list(msg = paste0("The message is: '", msg, "'"))
 }
 
-#* Plot a histogram
-#* @serializer png
-#* @get /plot
-function() {
-  rand <- rnorm(100)
-  hist(rand)
-}
-
 #* Health check
 #* @get /ping
 function() {
   list(status = "ok", time = format(Sys.time(), tz = "UTC"))
 }
-
-#* Return the sum of two numbers
-#* @usage curl "http://localhost:8787/echo?msg=hello"
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
-function(a, b) {
-  as.numeric(a) + as.numeric(b)
-}
-
 #* Run the model
 #* @post /run-model
 function(file_data, parameters) {
