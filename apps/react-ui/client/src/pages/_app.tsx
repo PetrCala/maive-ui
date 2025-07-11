bb9581fb73d35b426ca37e40e638bdb6fb627823
PetrCala <cala.p@seznam.cz>
2025-07-11 16:30:58 +0200

    feat: add a header with a home button

--- apps/react-ui/client/src/pages/_app.tsx
+++ apps/react-ui/client/src/pages/_app.tsx
@@ -3,10 +3,18 @@
 import { Providers } from "@providers/index"
 import "@styles/globals.css"
 import type { AppProps } from "next/app"
+import { usePathname } from "next/navigation"
+import Header from "@components/Header"
 
 export default function App({ Component, pageProps }: AppProps) {
+	const pathname = usePathname()
+
+	// Don't show header on the home page
+	const isHomePage = pathname === "/"
+
 	return (
 		<Providers>
+			{!isHomePage && <Header className="absolute top-4 right-4 z-50" />}
 			<Component {...pageProps} />
 		</Providers>
 	)
