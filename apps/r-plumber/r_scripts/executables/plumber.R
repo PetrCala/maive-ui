5bb6a370e5d0482e471925a13d8cad8e78434636
PetrCala <cala.p@seznam.cz>
2025-07-14 20:53:00 +0200

    feat: the R api now correctly calculates all significance stats

--- apps/r-plumber/r_scripts/executables/plumber.R
+++ apps/r-plumber/r_scripts/executables/plumber.R
@@ -94,20 +94,28 @@ function(file_data, parameters) {
   # This is the package response structure
   # maive_res <- list("beta" = round(beta, 3), "SE" = round(betase, 3), "F-test" = F_hac, "beta_standard" = round(beta0, 3), "SE_standard" = round(beta0se, 3), "Hausman" = round(Hausman, 3), "Chi2" = round(Chi2, 3), "SE_instrumented" = sebs2fit1^(1 / 2), "AR_CI" = b0_CI_AR)
 
+  get_est_is_significant <- function(est, se) {
+    if (se > 0) est / se >= 1.96 else TRUE
+  }
+
   est <- maive_res$beta
   se <- maive_res$SE
-  is_significant <- if (se > 0) est / se >= 1.96 else TRUE
+  est_is_significant <- get_est_is_significant(est, se)
+
+  pb_est <- maive_res$beta_standard
+  pb_se <- maive_res$SE_standard
+  pb_is_significant <- get_est_is_significant(pb_est, pb_se)
 
   # TODO: Add the rest of the results
   results <- list(
     effectEstimate = est,
     standardError = se,
-    isSignificant = is_significant,
+    isSignificant = est_is_significant,
     andersonRubinCI = maive_res$AR_CI, # c(int, int) or "NA"
     publicationBias = list(
-      estimate = maive_res$beta_standard,
-      standardError = maive_res$SE_standard,
-      isSignificant = FALSE
+      estimate = pb_est,
+      standardError = pb_se,
+      isSignificant = pb_is_significant
     ),
     firstStageFTest = maive_res[["F-test"]],
     hausmanTest = list(
