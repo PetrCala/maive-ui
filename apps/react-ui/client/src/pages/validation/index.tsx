4902863da736ff1e1f7bcaa265ae1d14e0a0bb08
PetrCala <cala.p@seznam.cz>
2025-07-16 17:59:51 +0200

    chore: reuse the alert component on the validation page

--- apps/react-ui/client/src/pages/validation/index.tsx
+++ apps/react-ui/client/src/pages/validation/index.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from "next/navigation"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useDataStore, dataCache } from "@store/dataStore"
+import Alert from "@src/components/Alert"
 
 interface ValidationMessage {
 	type: "success" | "error" | "warning" | "info"
@@ -385,74 +386,11 @@ export default function ValidationPage() {
 						</h2>
 						<div className="space-y-3">
 							{validationResult.messages.map((message, index) => (
-								<div
+								<Alert
 									key={index}
-									className={`p-4 rounded-lg border ${
-										message.type === "success"
-											? "bg-green-50 border-green-200 text-green-800 dark:bg-green-900/20 dark:border-green-800 dark:text-green-300"
-											: message.type === "error"
-											? "bg-red-50 border-red-200 text-red-800 dark:bg-red-900/20 dark:border-red-800 dark:text-red-300"
-											: message.type === "info"
-											? "bg-blue-50 border-blue-200 text-blue-800 dark:bg-blue-900/20 dark:border-blue-800 dark:text-blue-300"
-											: "bg-yellow-50 border-yellow-200 text-yellow-800 dark:bg-yellow-900/20 dark:border-yellow-800 dark:text-yellow-300"
-									}`}
-								>
-									<div className="flex items-center">
-										{message.type === "success" && (
-											<svg
-												className="w-5 h-5 mr-2"
-												fill="currentColor"
-												viewBox="0 0 20 20"
-											>
-												<path
-													fillRule="evenodd"
-													d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-													clipRule="evenodd"
-												/>
-											</svg>
-										)}
-										{message.type === "error" && (
-											<svg
-												className="w-5 h-5 mr-2"
-												fill="currentColor"
-												viewBox="0 0 20 20"
-											>
-												<path
-													fillRule="evenodd"
-													d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-													clipRule="evenodd"
-												/>
-											</svg>
-										)}
-										{message.type === "warning" && (
-											<svg
-												className="w-5 h-5 mr-2"
-												fill="currentColor"
-												viewBox="0 0 20 20"
-											>
-												<path
-													fillRule="evenodd"
-													d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-													clipRule="evenodd"
-												/>
-											</svg>
-										)}
-										{message.type === "info" && (
-											<svg
-												className="w-5 h-5 mr-2 text-blue-600 dark:text-blue-400"
-												fill="currentColor"
-												viewBox="0 0 20 20"
-											>
-												<path
-													fillRule="evenodd"
-													d="M18 10A8 8 0 11 2 10a8 8 0 0116 0zm-7-4a1 1 0 10-2 0 1 1 0 002 0zm-1 3a1 1 0 00-1 1v3a1 1 0 002 0v-3a1 1 0 00-1-1z"
-													clipRule="evenodd"
-												/>
-											</svg>
-										)}
-										<span className="font-medium">{message.message}</span>
-									</div>
-								</div>
+									message={message.message}
+									type={message.type}
+								/>
 							))}
 						</div>
 					</div>
