bec7cba6a0790ad7b364094f0c6c485a999a590e
PetrCala <cala.p@seznam.cz>
2025-07-16 17:26:22 +0200

    chore: add a config toggle for the model params help modal

---
+++ apps/react-ui/client/src/CONFIG.ts
@@ -0,0 +1,6 @@
+const CONFIG = {
+	WAIVE_ENABLED: false,
+	SHOULD_SHOW_MODEL_PARAMS_HELP_MODAL: false,
+} as const
+
+export default CONFIG
--- apps/react-ui/client/src/CONST.ts
+++ apps/react-ui/client/src/CONST.ts
@@ -1,5 +1,4 @@
 const CONST = {
-	WAIVE_ENABLED: false, // feature flag
 	MAIVE_URL: "https://meta-analysis.cz/maive/",
 	MAIVE_PAPER_URL: "https://meta-analysis.cz/maive/maive.pdf",
 	MAIVE_GITHUB_URL: "https://github.com/meta-analysis-es/maive",
