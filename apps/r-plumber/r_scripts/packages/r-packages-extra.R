66fb2fd8decf2c74c9b750e68c5121121c6492da
PetrCala <cala.p@seznam.cz>
2025-07-16 13:50:31 +0200

    ci: the r packages extra installation now installs the maive package locally

--- apps/r-plumber/r_scripts/packages/r-packages-extra.R
+++ apps/r-plumber/r_scripts/packages/r-packages-extra.R
@@ -7,7 +7,15 @@ maive_packages <- c(
 )
 install.packages(c(packages, maive_packages), dependencies = TRUE)
 
-# Install the MAIVE package
-maive_repo_name <- "meta-analysis-es/maive"
+# Install the MAIVE package - runs from the 'apps/r-plumber' directory
+install_local <- TRUE
 maive_tag_name <- "v0.1.0"
-devtools::install_github(repo = maive_repo_name, ref = maive_tag_name)
+
+if (install_local) {
+  cli::cli_alert_info("Installing MAIVE from local directory")
+  devtools::install_local(path = "vendor/maive")
+} else {
+  maive_repo_name <- "meta-analysis-es/maive"
+  cli::cli_alert_info("Installing MAIVE from GitHub")
+  devtools::install_github(repo = maive_repo_name, ref = maive_tag_name)
+}
