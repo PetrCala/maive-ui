a176fbea3e4065073c5c20c0260a1c58dfdcbaaf
PetrCala <cala.p@seznam.cz>
2025-07-16 14:27:43 +0200

    ci: the R extra packages installation can now install from the forked tag

--- apps/r-plumber/r_scripts/packages/r-packages-extra.R
+++ apps/r-plumber/r_scripts/packages/r-packages-extra.R
@@ -8,14 +8,17 @@ maive_packages <- c(
 install.packages(c(packages, maive_packages), dependencies = TRUE)
 
 # Install the MAIVE package - runs from the 'apps/r-plumber' directory
-install_local <- TRUE
-maive_tag_name <- "v0.1.0"
+install_forked <- TRUE # Whether to install from the forked repository
+maive_repo_name <- "maive"
 
-if (install_local) {
-  cli::cli_alert_info("Installing MAIVE from local directory")
-  devtools::install_local(path = "vendor/maive")
+if (install_forked) {
+  cli::cli_alert_info("Installing MAIVE from the forked repository")
+  forked_owner <- "PetrCala"
+  forked_tag_name <- "ui-v0.1.0"
+  devtools::install_github(repo = paste0(forked_owner, "/", maive_repo_name), ref = forked_tag_name)
 } else {
-  maive_repo_name <- "meta-analysis-es/maive"
-  cli::cli_alert_info("Installing MAIVE from GitHub")
-  devtools::install_github(repo = maive_repo_name, ref = maive_tag_name)
+  cli::cli_alert_info("Installing MAIVE from the original GitHub repository")
+  original_owner <- "meta-analysis-es"
+  original_tag_name <- "v0.1.0"
+  devtools::install_github(repo = paste0(original_owner, "/", maive_repo_name), ref = original_tag_name)
 }
