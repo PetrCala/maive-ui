28e4a964f3ff6a0d2bfc1be3207db1b16d55ae08
PetrCala <cala.p@seznam.cz>
2025-08-19 15:34:28 +0200

    ci: use the R load balancer private IP for the API URL

--- terraform/stacks/prod-runtime/ecs.tf
+++ terraform/stacks/prod-runtime/ecs.tf
@@ -37,7 +37,8 @@ resource "aws_ecs_task_definition" "ui" {
     image        = "${local.ecr_urls["react-ui"]}:${var.image_tag}"
     portMappings = [{ containerPort = local.ui_port, protocol = "tcp" }]
     environment = [
-      { name = "NEXT_PUBLIC_R_API_URL", value = "http://${aws_lb.r.dns_name}" },
+      # { name = "NEXT_PUBLIC_R_API_URL", value = "http://${aws_lb.r.dns_name}" }, # Using DNS name
+      { name = "NEXT_PUBLIC_R_API_URL", value = "http://${aws_lb.r.private_ip}" }, # Using IP address
     ]
     logConfiguration = {
       logDriver = "awslogs",
