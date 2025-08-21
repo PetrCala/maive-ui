39dcf7b64354f682e2e61c8c812f94392ec51b3d
PetrCala <cala.p@seznam.cz>
2025-08-21 09:57:54 +0200

    build: update the lambda dockerfile

--- lambda-r-backend/r_scripts/index.R
+++ lambda-r-backend/r_scripts/index.R
@@ -2,47 +2,27 @@
 # This replaces the plumber API with a Lambda function
 
 # Load required libraries
-library(jsonlite)
-library(cli)
+library(plumber)
 
-# Lambda handler function
-handler <- function(event, context) {
-  tryCatch(
-    {
-      # Parse the incoming event
-      http_method <- event$httpMethod
-      path <- event$path
-
-      # Route based on path and method
-      if (path == "/ping" && http_method == "GET") {
-        return(handle_ping())
-      } else if (path == "/run-model" && http_method == "POST") {
-        return(handle_run_model(event$body))
-      } else if (path == "/echo" && http_method == "GET") {
-        return(handle_echo(event$queryStringParameters$msg))
-      } else {
-        return(create_response(404, list(error = TRUE, message = "Endpoint not found")))
-      }
-    },
-    error = function(e) {
-      cli::cli_alert_danger("Lambda handler error: {e$message}")
-      create_response(500, list(error = TRUE, message = paste("Internal server error:", e$message)))
-    }
-  )
+#* Echo back the input
+#* @usage curl --data "a=4&b=3" "http://localhost:8787/sum"
+#* @param msg The message to echo
+#* @get /echo
+function(msg = "") {
+  list(msg = paste0("The message is: '", msg, "'"))
 }
 
-# Health check endpoint
-handle_ping <- function() {
-  create_response(200, list(status = "ok", time = format(Sys.time(), tz = "UTC")))
+#* Health check
+#* @get /ping
+function() {
+  list(status = "ok", time = format(Sys.time(), tz = "UTC"))
 }
 
-# Echo endpoint
-handle_echo <- function(msg = "") {
-  create_response(200, list(msg = paste0("The message is: '", msg, "'")))
-}
-
-# Main model execution endpoint
-handle_run_model <- function(body) {
+#* Run the model
+#* @param data The file data to run the model on, passed as a JSON string
+#* @param parameters The parameters to run the model on
+#* @post /run-model
+function(data, parameters) {
   tryCatch(
     {
       # nolint start: undesirable_function_linter.
@@ -61,34 +41,20 @@ handle_run_model <- function(body) {
       parameters <- body_data$parameters
 
       if (is.null(data) || is.null(parameters)) {
-        return(create_response(400, list(error = TRUE, message = "Missing data or parameters")))
+        return(list(error = TRUE, message = "Missing data or parameters"))
       }
 
-      # Run the MAIVE model
       results <- run_maive_model(data, parameters)
-
-      return(create_response(200, list(data = results)))
+      return(list(data = results))
     },
     error = function(e) {
-      cli::cli_alert_danger("Error in run-model: {e$message}")
-      create_response(500, list(error = TRUE, message = paste("Model execution error:", e$message)))
+      cli::cli_alert_danger("Error in run-model endpoint: {e$message}")
+      cli::cli_h2("Error traceback:")
+      cli::cli_code(capture.output(traceback()))
+      list(
+        error = TRUE,
+        message = paste("Internal server error:", e$message)
+      )
     }
   )
 }
-
-# Create HTTP response
-create_response <- function(status_code, body) {
-  list(
-    statusCode = status_code,
-    headers = list(
-      "Content-Type" = "application/json",
-      "Access-Control-Allow-Origin" = "*",
-      "Access-Control-Allow-Headers" = "Content-Type",
-      "Access-Control-Allow-Methods" = "GET,POST,OPTIONS"
-    ),
-    body = toJSON(body, auto_unbox = TRUE)
-  )
-}
-
-# Export the handler for Lambda
-exports.handler <- handler
