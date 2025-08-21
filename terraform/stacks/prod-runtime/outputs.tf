0ef9358809f717a0ad587f58a0738e00254a4ed0
PetrCala <cala.p@seznam.cz>
2025-08-21 13:56:48 +0200

    fix: tf config

--- terraform/stacks/prod-runtime/outputs.tf
+++ terraform/stacks/prod-runtime/outputs.tf
@@ -3,11 +3,6 @@ output "ui_alb_dns_name" {
   value       = aws_lb.ui.dns_name
 }
 
-output "r_alb_dns_name" {
-  description = "DNS name of the internal R ALB"
-  value       = aws_lb.r.dns_name
-}
-
 output "monitoring_dashboard_url" {
   description = "CloudWatch dashboard URL for monitoring"
   value       = "https://${var.region}.console.aws.amazon.com/cloudwatch/home?region=${var.region}#dashboards:name=${var.project}-dashboard"
@@ -15,5 +10,5 @@ output "monitoring_dashboard_url" {
 
 output "lambda_r_backend_url" {
   description = "URL of the Lambda R backend function"
-  value       = aws_lambda_function_url.r_backend.url
+  value       = aws_lambda_function_url.r_backend.function_url
 }
