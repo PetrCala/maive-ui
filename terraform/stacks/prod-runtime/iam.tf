eaeaed55419f80ad3c80ba4b6d7d29fc5a7561c2
PetrCala <cala.p@seznam.cz>
2025-08-21 14:45:12 +0200

    chore: remove redundant config

--- terraform/stacks/prod-runtime/iam.tf
+++ terraform/stacks/prod-runtime/iam.tf
@@ -1,5 +1,3 @@
-# UI-specific IAM role for UI task to invoke Lambda
-
 # Get current AWS account ID
 data "aws_caller_identity" "current" {}
 
@@ -8,19 +6,16 @@ resource "aws_iam_role" "ui_task" {
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks.json
 }
 
-# Attach basic execution policy
 resource "aws_iam_role_policy_attachment" "ui_task_exec" {
   role       = aws_iam_role.ui_task.name
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
 }
 
-# Attach SSM policy
 resource "aws_iam_role_policy_attachment" "ui_task_ssm" {
   role       = aws_iam_role.ui_task.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-# Custom policy for Lambda invocation
 resource "aws_iam_policy" "ui_task_lambda_invoke" {
   name        = "${var.project}-ui-task-lambda-invoke"
   description = "Allow UI task to invoke Lambda functions"
