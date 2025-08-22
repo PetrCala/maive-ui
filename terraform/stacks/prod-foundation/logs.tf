d5af75e18c9691800cd5a7d5b7e6eaaf3bba1af3
PetrCala <cala.p@seznam.cz>
2025-08-22 11:09:08 +0200

    fix: the r lambda logs now point to the correct log group

--- terraform/stacks/prod-foundation/logs.tf
+++ terraform/stacks/prod-foundation/logs.tf
@@ -4,8 +4,8 @@ resource "aws_cloudwatch_log_group" "ui_task" {
   tags              = { Project = var.project }
 }
 
-resource "aws_cloudwatch_log_group" "lambda_r_backend" {
-  name              = "/lambda/${var.project}/lambda-r-backend"
+resource "aws_cloudwatch_log_group" "lambda_default" {
+  name              = "/aws/lambda/${var.project}-lambda-r-backend"
   retention_in_days = var.log_retention_days
   tags              = { Project = var.project }
 }
--- terraform/stacks/prod-runtime/logs.tf
+++ terraform/stacks/prod-runtime/logs.tf
@@ -3,5 +3,5 @@ data "aws_cloudwatch_log_group" "ui_logs" {
 }
 
 data "aws_cloudwatch_log_group" "lambda_r_backend_logs" {
-  name = "/lambda/${var.project}/lambda-r-backend"
+  name = "/aws/lambda/${var.project}-lambda-r-backend"
 }
