cbb59128ad54e6c0552fd36643c953eb3c1368a8
PetrCala <cala.p@seznam.cz>
2025-08-22 16:25:07 +0200

    chore: lint and typescript are no longer checked at npm build

--- apps/react-ui/client/next.config.cjs
+++ apps/react-ui/client/next.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx"],
+	// We check both linting and type checking during CI/CD, but we don't need to check them during build.
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 }
 
 module.exports = nextConfig
