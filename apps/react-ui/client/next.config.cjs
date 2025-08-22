88b204e91ba528e85a16a270044f53cea7f8e937
PetrCala <cala.p@seznam.cz>
2025-08-22 16:27:48 +0200

    chore: remove the next config lint/typescript check values

--- apps/react-ui/client/next.config.cjs
+++ apps/react-ui/client/next.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx"],
-	// We check both linting and type checking during CI/CD, but we don't need to check them during build.
-	eslint: {
-		ignoreDuringBuilds: false,
-	},
-	typescript: {
-		ignoreBuildErrors: false,
-	},
 }
 
 module.exports = nextConfig
