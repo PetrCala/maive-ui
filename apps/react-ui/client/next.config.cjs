59e960a9f189b504d82c7480ba345f63f3cec808
PetrCala <cala.p@seznam.cz>
2025-08-22 16:25:30 +0200

    chore: disable lints and typescript checks

--- apps/react-ui/client/next.config.cjs
+++ apps/react-ui/client/next.config.cjs
@@ -3,10 +3,10 @@ const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx"],
 	// We check both linting and type checking during CI/CD, but we don't need to check them during build.
 	eslint: {
-		ignoreDuringBuilds: true,
+		ignoreDuringBuilds: false,
 	},
 	typescript: {
-		ignoreBuildErrors: true,
+		ignoreBuildErrors: false,
 	},
 }
 
