dd0c178a1b162072e5ef79e5fefb83ddb8a781fe
PetrCala <cala.p@seznam.cz>
2025-09-05 08:08:10 +0200

    refactor: move all modals into a single folder

---
+++ apps/react-ui/client/src/components/Modals/index.ts
@@ -0,0 +1,5 @@
+import MAIVEInfoModal from "./MAIVEInfoModal";
+import ParametersHelpModal from "./ParametersHelpModal";
+import RunInfoModal from "./RunInfoModal";
+
+export { MAIVEInfoModal, RunInfoModal, ParametersHelpModal };
