17587c287dae3bf7f9b3cab3c4f25d685b04b58a
PetrCala <cala.p@seznam.cz>
2025-11-04 14:34:21 +0900

    chore: modify the alb config and health checks

--- terraform/stacks/prod-runtime/alb.tf
+++ terraform/stacks/prod-runtime/alb.tf
@@ -14,14 +14,14 @@ resource "aws_lb_target_group" "ui" {
   protocol             = "HTTP"
   target_type          = "ip"
   vpc_id               = local.vpc_id
-  deregistration_delay = 300
+  deregistration_delay = 30
   health_check {
-    path                = "/"
+    path                = "/api/health"
     port                = "traffic-port"
     healthy_threshold   = 2
-    unhealthy_threshold = 3
-    timeout             = 10
-    interval            = 30
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 15
     matcher             = "200"
   }
 }
